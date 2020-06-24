1d65b70469deef1b63d3488e2db1446435613a66
Valerii Panin <v.panin@gsi.de>
2020-06-24 12:11:21 +0200

    Added geo file for 1 NeuLAND double plane + MDFWrapper class

--- tracking/TrackingLinkDef.h
+++ tracking/TrackingLinkDef.h
@@ -24,6 +24,7 @@
 #pragma link C++ class R3BFragmentTracker+;
 #pragma link C++ class R3BFragmentFitterGeneric+;
 #pragma link C++ class R3BFragmentFitterChi2+;
+#pragma link C++ class R3BMDFWrapper+;
 #pragma link C++ class R3BTrackingDetector+;
 #pragma link C++ class R3BTrackingParticle+;
 #pragma link C++ class R3BTrackingSetup+;
